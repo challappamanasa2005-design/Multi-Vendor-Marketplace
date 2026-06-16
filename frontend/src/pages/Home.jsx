800b03de1d0f9bda8a26e67ade012517cbd8e72d
Manasa Challappa <challappamanasa2005@gmail.com>
2026-06-16 15:05:09 +0530

    Initial project setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Multi-Vendor Marketplace</h1>
+      <p>Buy and sell products from multiple vendors.</p>
+    </div>
+  );
+}
+
+export default Home;
